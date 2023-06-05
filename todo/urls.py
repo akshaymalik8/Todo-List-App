fe453647e420b40a24b395b3759841a77dd8344b
beingajaat <malikakshay0420@gmail.com>
2023-06-05 16:49:34 +0530

    1.Created url of signup, login, homepage 2.Created views and user creation

--- todo/urls.py
+++ todo/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def home_page(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method=='POST':
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        password2=request.POST.get('password2')
+        if password==password2:
+            my_user = User.objects.create_user(email, password)
+            my_user.save()
+            return render(request, 'index.html')
+        else:
+            return render()
+    return render(request, 'signup.html')
